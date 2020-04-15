295cdb8a1a9962f54f4512839cc3c2e1f7a66519
linyiyuan <linyiyuann@163.com>
2020-04-15 23:44:06 +0800

    '对model进行分装'

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,4 +8,21 @@ class User extends Model
 {
     protected $table = 'user';
 
+    public static function getOneById($id)
+    {
+        if (empty($id)) return [];
+        return static::query()->where('id', $id)->first()->toArray();
+    }
+
+    public static function setUser($data, $id = '')
+    {
+        $result = static::query();
+        if (!empty($id)) {
+            $result = $result->where('id', $result)->first();
+        }
+        foreach ($data as $key => $val) {
+            $result->$key = $val;
+        }
+        return $result->save();
+    }
 }
